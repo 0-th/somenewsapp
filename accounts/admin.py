af64c2dee67c73264f3c4ef19355ffd720dd88cd
0-th <artemisfowlx@protonmail.com>
2023-03-15 12:23:01 +0100

    Modify custom admin to include age field

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,9 @@ class CustomAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ('email', 'username', 'age', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('age',)}),)
 
 
 admin.site.register(CustomUser, CustomAdmin)
