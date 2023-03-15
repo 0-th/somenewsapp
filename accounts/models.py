787bf91512c2a49959e7aafc55a2c99260400b76
0-th <artemisfowlx@protonmail.com>
2023-03-15 03:58:45 +0100

    Add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Custom user model"""
+    # Add an age field to the default fields from `AbstractUser`
+    age = models.PositiveIntegerField(null=True, blank=True)
+    # The age field can store a db entry as null and accepts an empty
+    # value as input (e.g form field input).
