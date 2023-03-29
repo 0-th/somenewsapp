44e3e845f4d9bcf8c77919c3591d1701527ad683
0-th <artemisfowlx@protonmail.com>
2023-03-29 14:53:27 +0100

    fix: use 3rd party package to manage secrets

--- config/settings.py
+++ config/settings.py
@@ -34,24 +34,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# Get config from .env file in dev and from env variables in prod 
-dev_secrets = {}
-
-try:
-    with open('../.env', 'r') as env_file:
-        lines = env_file.readlines()
-
-        for line in lines:
-            key, value = line.strip().split(sep='=')
-            dev_secrets[str(key.strip())] = str(value.strip())
-except FileNotFoundError:
-    pass
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=dev_secrets['SECRET_KEY'])
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = dev_secrets['DEBUG'] or False
+DEBUG = os.environ.get('DEBUG', default=0) in ['true', 't', '1']
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
@@ -111,7 +98,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(os.environ.get('DATABASE_URL', default=dev_secrets['DATABASE_URL']), conn_max_age=600)
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
 
 
